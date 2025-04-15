d6ae9a6d97196d1f911d36f2716ea138d9306a82
crizzo <cecilia.rizzo16@gmail.com>
2025-04-14 19:02:42 -0700

    Updates

---
+++ precache-manifest.e9b659eed35c05efa92175ebcc41ef2d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874d687fc4dd83029c7a8c4af4e9ead9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e6b5546d054a5e8b02",
+    "url": "/static/css/main.b29aae7f.chunk.css"
+  },
+  {
+    "revision": "a38231fa8cf2701bbedb",
+    "url": "/static/js/2.2b5afd34.chunk.js"
+  },
+  {
+    "revision": "26328c61e7e5db625dac4fff64d27cb8",
+    "url": "/static/js/2.2b5afd34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2124ed42f2f12e64bd",
+    "url": "/static/js/3.222e1668.chunk.js"
+  },
+  {
+    "revision": "76e6b5546d054a5e8b02",
+    "url": "/static/js/main.3c93d074.chunk.js"
+  },
+  {
+    "revision": "9016feca429f7a37279d",
+    "url": "/static/js/runtime-main.a649663b.js"
+  },
+  {
+    "revision": "36708a13a0736ea3b1fd9f4402b828db",
+    "url": "/static/media/Better Logo.36708a13.png"
+  },
+  {
+    "revision": "6388d24dc1b3e467c32a578485bacd0e",
+    "url": "/static/media/Brian.6388d24d.png"
+  },
+  {
+    "revision": "9e6dfab0ae94d4761e118252061dd844",
+    "url": "/static/media/Celia.9e6dfab0.jpg"
+  },
+  {
+    "revision": "53896b7308e39726542ca9e99a617b16",
+    "url": "/static/media/David.53896b73.png"
+  },
+  {
+    "revision": "133abf71ad2b3c171b7d89c9f80245d5",
+    "url": "/static/media/Developer Mode.133abf71.png"
+  },
+  {
+    "revision": "f9641581981107bd20f44e279109e2b4",
+    "url": "/static/media/Marc.f9641581.png"
+  },
+  {
+    "revision": "5ee473ee9502dcb38224dced771804aa",
+    "url": "/static/media/Monica.5ee473ee.jpeg"
+  },
+  {
+    "revision": "ce1dd5b3533fa42ea7c16bd5f9815ccf",
+    "url": "/static/media/Pin Extension.ce1dd5b3.png"
+  },
+  {
+    "revision": "eb3c34a8f9a2c5e44c163a14b32aae56",
+    "url": "/static/media/Product Page.eb3c34a8.png"
+  },
+  {
+    "revision": "6fd6c16e83d5df74f4580c73f3c69068",
+    "url": "/static/media/Screenshot 2025-03-30 at 8.22.10 AM.6fd6c16e.png"
+  },
+  {
+    "revision": "7a0e4bd39fedd8536cb46d026fdd4b9c",
+    "url": "/static/media/Select Extension.7a0e4bd3.png"
+  },
+  {
+    "revision": "4f01beaa062d20ccd5d7f2301addd4f3",
+    "url": "/static/media/Unzip folder.4f01beaa.png"
+  },
+  {
+    "revision": "23d4696adfc84e33651d22b261a26841",
+    "url": "/static/media/Working Extension.23d4696a.png"
+  }
+]);
